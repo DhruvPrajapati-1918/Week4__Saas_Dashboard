8f3080d8050574045c40404f66258b6a071e9130
DhruvPrajapati-1918 <dhruvprajapati1918@gmail.com>
2026-03-22 17:10:06 +0530

    Update data.js

--- javascript/data.js
+++ javascript/data.js
@@ -1,24 +1,24 @@
 
 export const mockDashboardData = [
 
-{ id:1,name:"Dhruv Prajapati",email:"Dhruv@gmail.com",revenue:3200,status:"Active",lastLogin:"2026-03-15"},
+{ id:1,name:"Dhruv Prajapati",email:"Dhruv@gmail.com",revenue:4500,status:"Active",lastLogin:"2026-03-15"},
 
-{ id:2,name:"Aryan Thakur",email:"Aryan@gmail.com",revenue:2100,status:"Pending",lastLogin:"2026-03-12"},
+{ id:2,name:"Aryan Thakur",email:"Aryan@gmail.com",revenue:4501,status:"Pending",lastLogin:"2026-03-12"},
 
-{ id:3,name:"Jiyanshi Prajapati",email:"Jiyanshi@gmail.com",revenue:4800,status:"Active",lastLogin:"2026-03-16"},
+{ id:3,name:"Jiyanshi Prajapati",email:"Jiyanshi@gmail.com",revenue:6900,status:"Active",lastLogin:"2026-03-16"},
 
-{ id:4,name:"Diya Gandhi",email:"Diya@gmail.com",revenue:1900,status:"Churned",lastLogin:"2026-02-28"},
+{ id:4,name:"Diya Gandhi",email:"Diya@gmail.com",revenue:7863,status:"Churned",lastLogin:"2026-02-28"},
 
-{ id:5,name:"Parth makhwana",email:"Parth@gmail.com",revenue:2700,status:"Active",lastLogin:"2026-03-10"},
+{ id:5,name:"Parth makhwana",email:"Parth@gmail.com",revenue:1256,status:"Active",lastLogin:"2026-03-10"},
 
-{ id:6,name:"Pooja Prajapati",email:"Pooja@gmail.com",revenue:3500,status:"Pending",lastLogin:"2026-03-11"},
+{ id:6,name:"Pooja Prajapati",email:"Pooja@gmail.com",revenue:1918,status:"Pending",lastLogin:"2026-03-11"},
 
-{ id:7,name:"Ankit Kumbi",email:"Ankit@gmail.com",revenue:4100,status:"Active",lastLogin:"2026-03-14"},
+{ id:7,name:"Ankit Kumbi",email:"Ankit@gmail.com",revenue:2547,status:"Active",lastLogin:"2026-03-14"},
 
-{ id:8,name:"Sagun Thakur",email:"Sagun@gmail.com",revenue:1500,status:"Churned",lastLogin:"2026-02-20"},
+{ id:8,name:"Sagun Thakur",email:"Sagun@gmail.com",revenue:5896,status:"Churned",lastLogin:"2026-02-20"},
 
-{ id:9,name:"Hetal Prajapati",email:"Hetal@gmail.com",revenue:2900,status:"Active",lastLogin:"2026-03-17"},
+{ id:9,name:"Hetal Prajapati",email:"Hetal@gmail.com",revenue:7854,status:"Active",lastLogin:"2026-03-17"},
 
-{ id:10,name:"Bhavana Prajapati",email:"Bhavana@gmail.com",revenue:3300,status:"Active",lastLogin:"2026-03-13"}
+{ id:10,name:"Bhavana Prajapati",email:"Bhavana@gmail.com",revenue:3145,status:"Active",lastLogin:"2026-03-13"}
 
 ];
