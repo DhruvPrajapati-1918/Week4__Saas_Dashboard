158e7cc32bd448f7f5cd68d1728677e9d710649f
DhruvPrajapati-1918 <dhruvprajapati1918@gmail.com>
2026-03-22 16:55:40 +0530

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+
+const toggle = document.getElementById("themeToggle")
+
+// load saved theme
+const savedTheme = localStorage.getItem("theme")
+if(savedTheme){
+document.documentElement.setAttribute("data-theme", savedTheme)
+}
+
+toggle.addEventListener("click", () => {
+
+const current = document.documentElement.getAttribute("data-theme")
+
+if(current === "light"){
+document.documentElement.setAttribute("data-theme","dark")
+localStorage.setItem("theme","dark")
+toggle.innerText = "🌙"
+}else{
+document.documentElement.setAttribute("data-theme","light")
+localStorage.setItem("theme","light")
+toggle.innerText = "☀️"
+}
+
+})
