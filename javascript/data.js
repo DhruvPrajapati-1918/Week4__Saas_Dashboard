8524b6e7e7f1ece95321380940ef864e55ee17e2
DhruvPrajapati-1918 <dhruvprajapati1918@gmail.com>
2026-03-23 13:57:05 +0530

    Update data.js

--- javascript/data.js
+++ javascript/data.js
@@ -1,24 +1,44 @@
-
 export const mockDashboardData = [
-
-{ id:1,name:"Dhruv Prajapati",email:"Dhruv@gmail.com",revenue:4500,status:"Active",lastLogin:"2026-03-15"},
-
-{ id:2,name:"Aryan Thakur",email:"Aryan@gmail.com",revenue:4501,status:"Pending",lastLogin:"2026-03-12"},
-
-{ id:3,name:"Jiyanshi Prajapati",email:"Jiyanshi@gmail.com",revenue:6900,status:"Active",lastLogin:"2026-03-16"},
-
-{ id:4,name:"Diya Gandhi",email:"Diya@gmail.com",revenue:7863,status:"Churned",lastLogin:"2026-02-28"},
-
-{ id:5,name:"Parth makhwana",email:"Parth@gmail.com",revenue:1256,status:"Active",lastLogin:"2026-03-10"},
-
-{ id:6,name:"Pooja Prajapati",email:"Pooja@gmail.com",revenue:1918,status:"Pending",lastLogin:"2026-03-11"},
-
-{ id:7,name:"Ankit Kumbi",email:"Ankit@gmail.com",revenue:2547,status:"Active",lastLogin:"2026-03-14"},
-
-{ id:8,name:"Sagun Thakur",email:"Sagun@gmail.com",revenue:5896,status:"Churned",lastLogin:"2026-02-20"},
-
-{ id:9,name:"Hetal Prajapati",email:"Hetal@gmail.com",revenue:7854,status:"Active",lastLogin:"2026-03-17"},
-
-{ id:10,name:"Bhavana Prajapati",email:"Bhavana@gmail.com",revenue:3145,status:"Active",lastLogin:"2026-03-13"}
-
+  {
+    id: "a1",
+    name: "Dhruv Prajapati",
+    email: "dhruv@gmail.com",
+    revenue: 1423.58,
+    status: "Active",
+    lastLogin: "may 22, 2022"
+  },
+  {
+    id: "a2",
+    name: "aryan thakur",
+    email: "aryan@gmail.com",
+    revenue: 2450.75,
+    status: "Pending",
+    lastLogin: "may 18, 2024"
+  },
+  {
+    id: "a3",
+    name: "Ankit kumbi",
+    email: "ankit@gmail.com",
+    revenue: 980.25,
+    status: "Churned",
+    lastLogin: "Apr 20, 2021"
+  },
+  {
+    id: "a4",
+    name: "Diya gandhi",
+    email: "diya@gmail.com",
+    revenue: 3100.00,
+    status: "Active",
+    lastLogin: "Apr 25, 2024"
+  },
+  {
+    id: "a5",
+    name: "Pooja Prajapati",
+    email: "Pooja@gmail.com",
+    revenue: 1870.40,
+    status: "Active",
+    lastLogin: "sep 19 , 2005"
+  }
 ];
+    
+   
